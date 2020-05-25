764da0f9d96f432c918e335190004398f9f25bfa
Julian Schick <julian.schick@posteo.de>
2020-05-25 22:54:10 +0200

    compat switch

--- src/flarm/flarmMap/FlarmMapWidget.cpp
+++ src/flarm/flarmMap/FlarmMapWidget.cpp
@@ -856,7 +856,11 @@ void FlarmMapWidget::paintLatLonGrid (QPainter &painter)
 
 		QString text=latitude.formatDmSuffix ("N", "S");
 		QPointF intersection;
+#ifdef QT_COMPAT
+        if (line.intersect (intersectionLine, &intersection)==QLineF::BoundedIntersection)
+#else
         if (line.intersects (intersectionLine, &intersection)==QLineF::BoundedIntersection)
+#endif
             drawText (painter, intersection, textAlignment, text);
 	}
 }
