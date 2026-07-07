5eb66d1425c4f8c807a38f154ba926427ec80c22
Julian Schick <julian.schick@posteo.de>
2026-07-07 10:57:08 +0200

    bugfix
    correctly start external weather rubies
    version 2.5.3

--- src/plugins/weather/ExternalWeatherPlugin.cpp
+++ src/plugins/weather/ExternalWeatherPlugin.cpp
@@ -47,15 +47,22 @@ void ExternalWeatherPlugin::refresh ()
 
 	if (isBlank (command)) OUTPUT_AND_RETURN (tr ("No command specified"));
 
-	QString commandProper;
-	QString parameters;
-	SkProcess::splitCommand (commandProper, parameters, command);
+    QString commandProper;
+    QString parameters;
+    SkProcess::splitCommand (commandProper, parameters, command);
 
-	QString resolved=resolveFilename (commandProper, Settings::instance ().pluginPaths);
+    QString resolved=resolveFilename (commandProper, Settings::instance ().pluginPaths);
 	if (isBlank (resolved)) OUTPUT_AND_RETURN (tr ("Command not found"));
 	if (!QFile::exists (resolved)) OUTPUT_AND_RETURN (tr ("Command does not exist"));
 
-	if (!process->startAndWait (resolved+notr (" ")+parameters)) OUTPUT_AND_RETURN (tr ("Error: %1").arg (process->getProcess ()->errorString ()));
+    QString resolvedWithArgs;
+    if (parameters.isEmpty()) {
+        resolvedWithArgs = resolved;
+    } else {
+        resolvedWithArgs = resolved + notr(" ") + parameters;
+    }
+
+    if (!process->startAndWait (resolvedWithArgs)) OUTPUT_AND_RETURN (tr ("Error: %1").arg (process->getProcess ()->errorString ()));
 	outputText (tr ("Process started"));
 }
 
